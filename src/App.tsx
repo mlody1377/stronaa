fda9827632295398c3d4db22d5751e7b85d40ac2
Karol <kkarolek0000@gmail.com>
2025-04-03 17:35:21 +0200

    ogolnie to jest problemxd

--- src/App.tsx
+++ src/App.tsx
@@ -4,86 +4,75 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div class="profile-container">
-        <div class="profile-header">
-          <img src="assets/sigma.png" alt="Profile Picture" />
-          <h2>Mlody</h2>
-          <div class="tags">
-            <div class="tag">
-              <a
-                href="https://steamcommunity.com/id/Superwow134/"
-                target="_blank"
-              >
-                <img
-                  src="assets/Steam.png"
-                  alt="Steam obrazek"
-                  class="obrazek"
-                />
-                Steam
-                <img
-                  src="assets/arrow.png"
-                  alt="arrow obrazek"
-                  class="obrazek"
-                />
-              </a>
-            </div>
-            <div class="tag">
-              <a href="https://wakatime.com/@mlodysigma" target="_blank">
-                <img
-                  src="assets/wakatime.svg"
-                  alt="wakatime obrazek"
-                  class="obrazek"
-                />{" "}
-                WakaTime
-                <img
-                  src="assets/arrow.png"
-                  alt="arrow obrazek"
-                  class="obrazek"
-                />
-              </a>
-            </div>
-            <div class="tag">
-              <a href="https://github.com/karolturecki" target="_blank">
-                <img
-                  src="assets/github.png"
-                  alt="github obrazek"
-                  class="obrazek"
-                />{" "}
-                GitHub
-                <img
-                  src="assets/arrow.png"
-                  alt="arrow obrazek"
-                  class="obrazek"
-                />{" "}
-              </a>
-            </div>
+
+
+return (
+  <>
+    <div class="profile-container">
+      <div class="profile-header">
+        <img src="assets/sigma.png" alt="Profile Picture" />
+        <h2>Mlody</h2>
+        <div class="tags">
+          <div class="tag">
+            <a
+              href="https://steamcommunity.com/id/Superwow134/"
+              target="_blank"
+	      >
+              <img src="assets/Steam.png" alt="Steam obrazek" class="obrazek" />
+              Steam
+              <img src="assets/arrow.png" alt="arrow obrazek" class="obrazek" />
+            </a>
+          </div>
+          <div class="tag">
+            <a href="https://wakatime.com/@mlodysigma" target="_blank">
+              <img
+                src="assets/wakatime.svg"
+                alt="wakatime obrazek"
+                class="obrazek"
+              />{" "}
+              WakaTime 
+              <img src="assets/arrow.png" alt="arrow obrazek" class="oibrazek" />
+            </a>
+          </div>
+          <div class="tag">
+            <a™ href="https://github.com/karolturecki" target="_blank">
+              <img
+                src="assets/github.png"
+                alt="github obrazek"
+                class="obrazek"
+              />{" "}
+              GitHub
+              <img
+                src="assets/arrow.png"
+                alt="arrow obrazek"
+                class="obrazek"
+              />{" "}
+            </a>
           </div>
         </div>
-        <div></div>
-        <div id="about-me" class="about-me">
-          <a class="a" href="stronka.github.io/about" target="_blank">
-            About me{" "}
-            <img src="assets/arrow.png" alt="arrow obrazek" class="strzalka" />
-          </a>
-        </div>
-        <div class="now-playing">
-          <img
-            id="album-art"
-            class="album-art"
-            src=""
-            alt="Album Art"
-            style="display: none;"
-          />
-          <span>Currently Playing:</span>
-          <span id="current-track">Loading...</span>
-        </div>
       </div>
-      <script src="script.js"></script>
-    </>
-  );
+      <div></div>
+      <div id="about-me" class="about-me">
+        <a class="a" href="stronka.github.io/about" target="_blank">
+          About me{" "}
+          <img src="assets/arrow.png" alt="arrow obrazek" class="strzalka" />
+        </a>
+      </div>
+      <div class="now-playing">
+        <img
+          id="album-art"
+          class="album-art"
+          src=""
+          alt="Album Art"
+          style="display: none;"
+        />
+        <span>Currently Playing:</span>
+        <span id="current-track">Loading...</span>
+      </div>
+    </div>
+    <script src="script.js"></script>
+  </>
+);
 }
 export default App;
